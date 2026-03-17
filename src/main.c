b1a99a6f1f41b34d02c70061028eae9b773d802c
Jackson Wong <wjxa20152015@gmail.com>
2026-03-17 22:17:19 +0800

    test threads

--- src/main.c
+++ src/main.c
@@ -1,117 +1,94 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
-#include <zephyr/drivers/display.h>
 #include <zephyr/drivers/sensor.h>
-#include <zephyr/drivers/flash.h>
 #include <zephyr/logging/log.h>
-#include <string.h>
 
 LOG_MODULE_REGISTER(main);
 
-/* Aliases from devicetree */
-#define LED0_NODE  DT_ALIAS(led0)
-#define OLED_NODE  DT_ALIAS(oled0)
-#define BME280_NODE DT_ALIAS(temp0)
-#define FLASH_NODE DT_ALIAS(flash0)
+/* --- Configuration --- */
+#define SENSOR_SLEEP_MS 2000
+#define STACK_SIZE 1024
+#define PRIORITY 7
 
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-static const struct device *display = DEVICE_DT_GET(OLED_NODE);
-static const struct device *bme280  = DEVICE_DT_GET(BME280_NODE);
-static const struct device *flash   = DEVICE_DT_GET(FLASH_NODE);
+/* --- Data Structure --- */
+struct sensor_data {
+	float temp;
+	float press;
+	float hum;
+};
 
-void test_flash(void)
-{
-	const uint32_t offset = 0x000000;
-	const uint8_t write_data[] = {0xDE, 0xAD, 0xBE, 0xEF};
-	uint8_t read_data[4];
-	int ret;
+/* --- Message Queue --- */
+/* Define a queue that can hold 10 'sensor_data' items */
+K_MSGQ_DEFINE(sensor_msgq, sizeof(struct sensor_data), 10, 4);
 
-	LOG_INF("--- Flash Test ---");
+/* --- Hardware Specs --- */
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+static const struct device *bme280  = DEVICE_DT_GET(DT_ALIAS(temp0));
 
-	ret = flash_erase(flash, offset, 4096); // Sector erase
-	if (ret != 0) {
-		LOG_ERR("Flash erase failed: %d", ret);
-		return;
-	}
-
-	ret = flash_write(flash, offset, write_data, sizeof(write_data));
-	if (ret != 0) {
-		LOG_ERR("Flash write failed: %d", ret);
-		return;
-	}
-
-	ret = flash_read(flash, offset, read_data, sizeof(read_data));
-	if (ret != 0) {
-		LOG_ERR("Flash read failed: %d", ret);
-		return;
-	}
-
-	if (memcmp(write_data, read_data, sizeof(write_data)) == 0) {
-		LOG_INF("Flash Test PASSED: %02X %02X %02X %02X", 
-			read_data[0], read_data[1], read_data[2], read_data[3]);
-	} else {
-		LOG_ERR("Flash Test FAILED: %02X %02X %02X %02X", 
-			read_data[0], read_data[1], read_data[2], read_data[3]);
-	}
-}
-
-void test_sensors(void)
+/* --- Producer Thread: Sensor Reading --- */
+void sensor_thread_entry(void *p1, void *p2, void *p3)
 {
 	struct sensor_value temp, press, hum;
-	int ret;
+	struct sensor_data data;
 
-	LOG_INF("--- Sensor Test ---");
-
-	ret = sensor_sample_fetch(bme280);
-	if (ret != 0) {
-		LOG_ERR("BME280 fetch failed: %d", ret);
+	if (!device_is_ready(bme280)) {
+		LOG_ERR("BME280 not ready");
 		return;
 	}
 
-	sensor_channel_get(bme280, SENSOR_CHAN_AMBIENT_TEMP, &temp);
-	sensor_channel_get(bme280, SENSOR_CHAN_PRESS, &press);
-	sensor_channel_get(bme280, SENSOR_CHAN_HUMIDITY, &hum);
+	while (1) {
+		if (sensor_sample_fetch(bme280) == 0) {
+			sensor_channel_get(bme280, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+			sensor_channel_get(bme280, SENSOR_CHAN_PRESS, &press);
+			sensor_channel_get(bme280, SENSOR_CHAN_HUMIDITY, &hum);
 
-	LOG_INF("BME280: Temp: %d.%06d C, Press: %d.%06d kPa, Hum: %d.%06d %%",
-		temp.val1, temp.val2, press.val1, press.val2, hum.val1, hum.val2);
+			data.temp  = (float)sensor_value_to_double(&temp);
+			data.press = (float)sensor_value_to_double(&press);
+			data.hum   = (float)sensor_value_to_double(&hum);
+
+			/* Send to queue (Wait forever if full) */
+			k_msgq_put(&sensor_msgq, &data, K_FOREVER);
+			LOG_DBG("Producer: Sent data to queue");
+		}
+		
+		k_msleep(SENSOR_SLEEP_MS);
+	}
 }
 
-int main(void)
+/* --- Consumer Thread: Data Processing & LED --- */
+void processing_thread_entry(void *p1, void *p2, void *p3)
 {
-	LOG_INF("--- App Start (Full Port) ---");
+	struct sensor_data received_data;
 
 	if (!gpio_is_ready_dt(&led)) {
-		LOG_ERR("LED device not ready");
-	} else {
-		gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-		LOG_INF("LED ready on PB2");
+		LOG_ERR("LED not ready");
+		return;
 	}
-
-	if (!device_is_ready(display)) {
-		LOG_ERR("Display device not ready!");
-	} else {
-		LOG_INF("Display ready");
-	}
-
-	if (!device_is_ready(bme280)) {
-		LOG_ERR("BME280 device not ready!");
-	} else {
-		LOG_INF("BME280 ready");
-	}
-
-	if (!device_is_ready(flash)) {
-		LOG_ERR("Flash device not ready!");
-	} else {
-		LOG_INF("Flash ready (W25Q16)");
-	}
-
-	test_flash();
+	gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 
 	while (1) {
-		test_sensors();
-		gpio_pin_toggle_dt(&led);
-		k_msleep(2000);
+		/* Wait for data from queue */
+		if (k_msgq_get(&sensor_msgq, &received_data, K_FOREVER) == 0) {
+			LOG_INF("Consumer: T=%.2f C, P=%.2f kPa, H=%.2f %%",
+				(double)received_data.temp, 
+				(double)received_data.press, 
+				(double)received_data.hum);
+			
+			gpio_pin_toggle_dt(&led);
+		}
 	}
+}
 
+/* --- Static Thread Definitions --- */
+K_THREAD_DEFINE(sensor_tid, STACK_SIZE, sensor_thread_entry, NULL, NULL, NULL,
+		PRIORITY, 0, 0);
+
+K_THREAD_DEFINE(processing_tid, STACK_SIZE, processing_thread_entry, NULL, NULL, NULL,
+		PRIORITY, 0, 0);
+
+/* Main thread can stay idle or do other things */
+int main(void)
+{
+	LOG_INF("Zephyr Multi-threaded App Started");
 	return 0;
 }
