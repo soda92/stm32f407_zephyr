210f02bba510956450ee6457c47001257a0ccbcc
Jackson Wong <wjxa20152015@gmail.com>
2026-03-17 21:33:44 +0800

    init oled

--- src/main.c
+++ src/main.c
@@ -1,38 +1,61 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/display.h>
 #include <zephyr/logging/log.h>
+#include <string.h>
 
 LOG_MODULE_REGISTER(main);
 
-/* The devicetree node identifier for the "led0" alias. */
+/* Aliases from devicetree */
 #define LED0_NODE DT_ALIAS(led0)
-
-#if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
-#error "Unsupported board: led0 devicetree alias is not defined"
-#endif
+#define OLED_NODE DT_ALIAS(oled0)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct device *display = DEVICE_DT_GET(OLED_NODE);
+
+void oled_flash_test(const struct device *dev)
+{
+	struct display_buffer_descriptor desc;
+	uint8_t buffer[1024]; // 128x64 pixels / 8 bits per pixel
+
+	desc.width = 128;
+	desc.height = 64;
+	desc.pitch = 128;
+
+	while (1) {
+		// --- WHITE ---
+		memset(buffer, 0xFF, sizeof(buffer));
+		LOG_INF("OLED White");
+		display_write(dev, 0, 0, &desc, buffer);
+		k_msleep(1000);
+
+		// --- BLACK ---
+		memset(buffer, 0x00, sizeof(buffer));
+		LOG_INF("OLED Black");
+		display_write(dev, 0, 0, &desc, buffer);
+		k_msleep(1000);
+
+		// Toggle LED too
+		gpio_pin_toggle_dt(&led);
+	}
+}
 
 int main(void)
 {
-	int ret;
-
 	if (!gpio_is_ready_dt(&led)) {
+		LOG_ERR("LED device not ready");
 		return 0;
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
+	if (!device_is_ready(display)) {
+		LOG_ERR("Display device not ready");
 		return 0;
 	}
 
-	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
-		}
-		LOG_INF("Blink!");
-		k_msleep(1000);
-	}
+	gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	
+	LOG_INF("Starting OLED Flash Test...");
+	oled_flash_test(display);
+
 	return 0;
 }
